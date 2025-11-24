dc2007023844d131f25bc8dad89b28e9bdeb49fd
Nirajan <nirajandevhub108@gmail.com>
2025-11-24 06:46:12 +0000

    devops shell scripting day-1

---
+++ day1/info.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+######################
+# author : nirajan singh
+######################
+
+echo
+echo "==============================="
+
+echo "username"
+whoami
+
+echo "Today's date"
+date
+
+echo " working directory"
+pwd
+
+echo "==============================="
+
