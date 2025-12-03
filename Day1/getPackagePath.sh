0b740efbdc1615d45e77d347d4d6d99b66f000cc
Nirajan <nirajandevhub108@gmail.com>
2025-12-03 10:00:21 +0000

    added chaining command comments argumenst

--- Day1/getPackagePath.sh
+++ Day1/getPackagePath.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
- 
+
+# write a shell script to find the pkg/file location path
+
+
 pakgName=$1
 
 command -v ${pakgname} && command -v ${pakgName} || echo "Package ${pakgName} is not found"
