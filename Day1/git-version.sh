b5980aa3b9dcaadf3d533c6bd2d94434db8a5a15
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-22 21:55:16 +0530

    add success message and display latest Git version in script

--- Day1/git-version.sh
+++ Day1/git-version.sh
@@ -6,7 +6,9 @@ downFile=gitVersionInfo_$(date '+%F%T')
 httpCode=$(curl -s -w '%{http_code}' ${gitCersionSourceUrl} -o ${downFile})
 
 if [[ ${httpCode} -eq 200 ]]; then
-    grep -oE '\-[0-9]+\.[0-9]+\.[0-9]+\.tar.gz' ${downFile} | grep -oE '[0-9]+\.[0-9]+\.[0-9]+' | sort --version-sort --field-separator=. | uniq | tail -1
+    echo "Succesfully downloaded version info"
+    latestversion=$(grep -oE '\-[0-9]+\.[0-9]+\.[0-9]+\.tar.gz' ${downFile} | grep -oE '[0-9]+\.[0-9]+\.[0-9]+' | sort --version-sort --field-separator=. | uniq | tail -1)
+    echo " latest version is :${latestversion}"
 else
     echo "Failed to  get version info"
 fi
