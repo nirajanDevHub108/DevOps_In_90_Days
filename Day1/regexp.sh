6974e04dbd16edcf3585fe907502ec0466f54e53
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-22 22:11:31 +0530

    add support for package installation across different OS types in regexp.sh

--- Day1/regexp.sh
+++ Day1/regexp.sh
@@ -1,5 +1,14 @@
 #!/opt/homebrew/bin/bash
 read -rp "enter your pakcgae namge :" pkgName
-
+osType=$(uname -s)
 echo " installimg yoy pkg: ${pkgName}"
-brew install ${pkgName} 
+brew install ${pkgName}
+case ${osType} in
+      rhel) yum install ${pkgName} -y ;;
+      centos) yum install ${pkgName} -y ;;
+      ubuntu) apt-get install ${pkgName} -y ;;
+      macOS) brew install ${pkgName} ;;
+      *) echo "invalid pakage name we have update this script to work with ${osType}" ;;
+esac
+
+sw_vers -productName
