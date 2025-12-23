e60333fb73be4094cdf2206431b9c52888177b51
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-23 11:40:46 +0530

    nllyfying the python module script

--- Day1/pythonModule.sh
+++ Day1/pythonModule.sh
@@ -1,7 +1,7 @@
 #!/opt/homebrew/bin/bash
 
 moduleName=boto3
-python3 -c "import ${moduleName}"
+python3 -c "import ${moduleName}" 1>/dev/null 2>/dev/null
 if [[ $? -eq 0 ]]; then
     echo " The ${moduleName} module is installed"
 else
