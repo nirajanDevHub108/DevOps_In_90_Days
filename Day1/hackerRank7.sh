761e4ab79fc0bec4de41143b8c69f3076f4cc21a
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-23 12:02:34 +0530

    feat-solved hacker rank conditional shell scripting problem no 7

---
+++ Day1/hackerRank7.sh
@@ -0,0 +1,35 @@
+<<PURPOSE
+Read in one character from STDIN.
+If the character is 'Y' or 'y' display "YES".
+If the character is 'N' or 'n' display "NO".
+No other character will be provided as input.
+
+Input Format
+
+One character
+
+Constraints
+
+The character will be from the set .
+
+Output Format
+
+echo YES or NO to STDOUT.
+
+Sample Input
+
+y  
+Sample Output
+
+YES
+PURPOSE
+
+#!/opt/homebrew/bin/bash
+
+read -p "enter charcter :" -n 1 char
+
+if [[ ${char} == 'Y' ]] ||[[ ${char} == 'y' ]] ; then
+    echo "YES"
+else
+    echo "NO"
+fi
