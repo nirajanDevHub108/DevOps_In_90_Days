35c0784581704b740c5d86fb730e17dafe9c089f
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-23 12:14:31 +0530

    prob-hackerrank-8 using condition to check three value with and or operators

---
+++ Day1/hackerRank8.sh
@@ -0,0 +1,24 @@
+#!/opt/homebrew/bin/bash
+# Given three integers (, , and ) representing the three sides of a triangle, identify whether the triangle is scalene, isosceles, or equilateral.
+
+# If all three sides are equal, output EQUILATERAL.
+# Otherwise, if any two sides are equal, output ISOSCELES.
+# Otherwise, output SCALENE.
+# Input Format
+# Three integers, each on a new line.
+# Constraints
+
+# The sum of any two sides will be greater than the third.
+# Output Format
+# One word: either "SCALENE" or "EQUILATERAL" or "ISOSCELES" (quotation marks excluded)
+read X
+read Y
+read Z
+
+if [[ ${X} -eq ${Y} ]] && [[ ${X} -eq ${Z} ]] && [[ ${Y} -eq ${Z} ]] ; then
+    echo "EQUILATERAL"
+elif [[ ${X} -eq ${Y} ]] || [[ ${X} -eq ${Z} ]] || [[ ${Y} -eq ${Z} ]]; then
+    echo "ISOSCELES"
+else
+    echo "SCALENE"
+fi
