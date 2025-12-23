442126e0f1ab28adc23d39d64a05c424a7b5025f
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-23 11:41:37 +0530

    feat-added using command line value

--- Day1/pythonModule.sh
+++ Day1/pythonModule.sh
@@ -1,6 +1,6 @@
 #!/opt/homebrew/bin/bash
 
-moduleName=boto3
+moduleName=${1}
 python3 -c "import ${moduleName}" 1>/dev/null 2>/dev/null
 if [[ $? -eq 0 ]]; then
     echo " The ${moduleName} module is installed"
